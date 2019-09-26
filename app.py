1ccc2778d83bc6dad7d2cb84d878445d681936a2
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-09-26 10:41:31 -0700

    users can see all mock playlists

--- app.py
+++ app.py
@@ -2,10 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# OUR MOCK ARRAY OF PROJECTS
+playlists = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+]
+
 @app.route('/')
-def index():
-    """Return homepage."""
-    return render_template('home.html', msg="Flask is cool")
+def playlists_index():
+    """Show all playlists."""
+    return render_template('playlists_index.html', playlists=playlists)
+
+# @app.route('/playlists/new', methods=['GET'])
+# @app.route('/playlists', methods=['POST'])
+# @app.route('/playlists/:id', methods=['GET'])
+# @app.route('/playlists/:id/edit', methods=['GET'])
+# @app.route('/playlists/:id', methods=['PUT'])
+# @app.route('/playlists/:id', methods=['DELETE'])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
